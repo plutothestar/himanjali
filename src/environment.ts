6f207d922ed05491b63584a7cde4666a3b03666d
ayushmalviya-sdn <ayushmalviya@smartdatainc.net>
2025-05-09 11:18:42 +0530

    some changes

--- src/environment.ts
+++ src/environment.ts
@@ -3,6 +3,9 @@ export const environment = {
   vercel: {
     ApiUrl: 'https://himacuity.vercel.app/api',
   },
+  gitHub: {
+    ApiUrl: 'https://api.github.com/repos/plutothestar/himacms/contents/',
+  },
   availablityCalender:'2595d4bfcf2f2b790e73b9775c011460d22aece0bd463cfffdc5c21563013485@group.calendar.google.com',
   appointmentCalender:'1d94c4c882ea6f4754d7a97fb2ad865b79379330c02fbb07aa542bf17301a91a@group.calendar.google.com',
   currentHost: `${window.location.protocol}//${window.location.hostname}`,
