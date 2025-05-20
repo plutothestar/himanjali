ced6e80d7d38bdba4b43463314408c92bc5721f0
ayushmalviya-sdn <ayushmalviya@smartdatainc.net>
2025-05-20 16:39:47 +0530

    some more changes

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { BlogService } from '../../services/blog.service';
+import { BlogMeta } from '../../shared/blog-meta';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule],
+  imports: [CommonModule,RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {}
+export class HomeComponent {
+  blogs: BlogMeta[] = [];
+
+  constructor(private blogService: BlogService) {}
+
+  ngOnInit(): void {
+    this.blogService.getBlogs().subscribe(data => this.blogs = data);
+  }
+}
--- src/app/shared/blog-meta.ts
+++ src/app/shared/blog-meta.ts
@@ -4,4 +4,5 @@ export interface BlogMeta {
     date: string;
     author: string;
     image: string;
+    comments: string;
 }
