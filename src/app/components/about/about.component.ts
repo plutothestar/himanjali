b5fecbc7f813abf03fa641f8592b60fc276f60de
ayushmalviya-sdn <ayushmalviya@smartdatainc.net>
2025-05-29 00:07:13 +0530

    some updates

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -10,10 +10,8 @@ import { RouterLink } from '@angular/router';
 })
 export class AboutComponent {
   images: string[] = [
-    'https://images.unsplash.com/photo-1509023464722-18d996393ca8?q=80&w=1470&auto=format&fit=crop',
-    'https://plus.unsplash.com/premium_photo-1661927190802-97d541e1d182?q=80&w=1471&auto=format&fit=crop',
-    'https://images.unsplash.com/photo-1601138061518-864051769be7?q=80&w=1471&auto=format&fit=crop',
-
+    '/images/certificate1.jpeg',
+    '/images/certificate2.jpeg',
   ];
 
   lightboxOpen: boolean = false;
